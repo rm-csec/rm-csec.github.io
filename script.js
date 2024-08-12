255361d4d79b2c4734642e341790ac9bc1d30582
0xL30N3 <dudepro1111@gmail.com>
2024-08-11 23:31:10 -0400

    test render of MD file

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-import {MarkdownBlock, MarkdownSpan, MarkdownElement} from "https://md-block.verou.me/md-block.js";
+fetch('/md/VikeCTF.md')
+  .then(response => response.text())
+  .then(data => {
+    document.getElementById("render").innerHTML = data; 
+  })
+  .catch(error => console.error('Error fetching the file:', error));
