f9135b90639e26ff3ba76c5dc563e957636c6f84
0xL30N3 <dudepro1111@gmail.com>
2024-08-12 00:51:47 -0400

    performance fixes

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+import { marked } from "https://cdn.jsdelivr.net/npm/marked/lib/marked.esm.js";
 async function fetchData() {
   try {
     const f = await fetch('/md/VikeCTF.md');
     const data = await f.text();
-    document.getElementById('render').innerHTML = data;
+    document.getElementById('render').innerHTML = marked.parse(data);
   }catch (error){
     document.getElementById('render').innerText = 'An error occurred while fetching data.';
     console.error('Error fetching data:', error);
